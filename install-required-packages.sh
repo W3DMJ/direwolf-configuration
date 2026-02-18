27bf2dd480046b91f8832ec013b9ff695b178cc3
Daniel Metcalf <w3dmj.dan@gmail.com>
2026-02-18 15:42:15 -0600

    Inital work to install required packages, extract direwolf githib repository, compile and install direwolf and initial configuration files.

--- install-required-packages.sh
+++ install-required-packages.sh
@@ -1,2 +1,27 @@
 #! /bin/bash
 echo "Installing required packages to compile and run direwolf TNC and ax25"
+
+# Install everything required to compile direwolf
+sudo apt install git git-lfs cmake build-essential libgps-dev libhamlib-dev libgpiod-dev libasound2-dev libudev-dev -y
+
+# Create area to build direwolf
+echo "Creating ~/Source/repos/amateur-radio-projects/ for cloning and compiling the direwolf github repository"
+mkdir -p ~/Source/repos/amateur-radio-projects
+cd ~/Source/repos/amateur-radio-projects
+
+# Extracting the direwolf repo from github
+git clone https://github.com/wb2osz/direwolf.git
+
+# Setup the build directory and compile
+cd direwolf
+mkdir build
+cd build
+cmake ..
+make
+
+# Perform install of binaries and have installer
+# generate the initial configuration files. Initial
+# configutation files are placed in the user's home 
+# directory
+sudo make install
+make install-conf
